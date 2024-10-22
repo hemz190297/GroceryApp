d7aa4d440e383d7026cac73909faa85311094afb
MouliMohanN <moulimohann@gmail.com>
2024-10-22 20:06:48 +0530

    configured prettier

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,8 @@
 module.exports = {
-  arrowParens: 'avoid',
-  bracketSameLine: true,
-  bracketSpacing: false,
+  experimentalTernaries: true,
+  tabWidth: 2,
+  useTabs: false,
   singleQuote: true,
-  trailingComma: 'all',
+  jsxSingleQuote: true,
+  printWidth: 120
 };
