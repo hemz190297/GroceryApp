3205bc59edf57b15c89ded0d22eeb17c7179f995
MouliMohanN <moulimohann@gmail.com>
2024-10-24 12:04:03 +0530

    eslint config all, unable to override the plugin rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,28 +4,52 @@ import tseslint from 'typescript-eslint';
 import pluginReact from 'eslint-plugin-react';
 
 export default [
-  { 
-    files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] 
+  {
+    files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}']
   },
-  { 
-    languageOptions: { 
+  {
+    languageOptions: {
       parserOptions: {
         projectService: true,
         tsconfigRootDir: import.meta.dirname,
       },
-      globals: { ...globals.browser, ...globals.node } 
-    } 
+      globals: { ...globals.browser, ...globals.node }
+    }
   },
+  // pluginJs.configs.all,
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   ...tseslint.configs.strict,
   ...tseslint.configs.stylistic,
+
+  /* ...tseslint.configs.all,
+  {
+    rules: {
+      "@typescript-eslint/explicit-function-return-type": "off",
+      "overrides": [
+        {
+          // enable the rule specifically for TypeScript files
+          "files": ["*.ts", "*.mts", "*.cts"],
+          "rules": {
+            "@typescript-eslint/explicit-function-return-type": "error",
+          },
+        },
+      ],
+    }
+  }, */
   pluginReact.configs.flat.recommended,
   {
     ignores: [
       ".detoxrc-ci.js", ".eslintrc.js", ".prettierrc.js",
       ".node_modules/", ".git/",
-      "**/.config/",
+      "**/*.config.js", " **/*.config.mjs",
     ]
-  }
+  },
+  {
+    rules: {
+      "no-unused-vars": "error",
+      "no-undef": "error",
+    },
+  },
+
 ];
