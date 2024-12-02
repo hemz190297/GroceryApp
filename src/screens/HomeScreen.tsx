5845a17fa28a244b4d7f101a516a12aac368787d
MouliMohanN <moulimohann@gmail.com>
2024-12-02 09:42:47 +0530

    fix navigation type

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,8 +1,14 @@
+import { ParamListBase } from '@react-navigation/native';
+import { NativeStackNavigationProp } from '@react-navigation/native-stack';
 import React from 'react';
 import {ScrollView, Text, TouchableOpacity, View} from 'react-native';
 import {Button} from 'react-native-paper';
 
-export const HomeScreen = ({navigation}) => {
+export interface HomeScreenProps {
+  navigation: NativeStackNavigationProp<ParamListBase>;
+}
+
+export const HomeScreen: React.FC<HomeScreenProps> = ({navigation}) => {
   const renderButton = (title: string, onPress: () => void) => {
     return (
       <Button mode="contained" style={{ marginTop: 16}} onPress={onPress}>
